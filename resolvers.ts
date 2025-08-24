4e503a6d914bfcf5de9382c6210a6133487f81a5
buiminhdien <dienbui2005@gmail.com>
2025-08-24 12:20:09 +0700

    lấy ra danh sách bài viết

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,15 @@
+import Article from "./models/article.models"
+
 export const resolvers = {
   Query:{
     hello : () =>{
       return "hello world"
+    },
+    getListArticle : async () =>{
+      const article = await Article.find({
+        deleted:false
+      })
+      return article
     }
   }
 }
